43823502243da2f921dbd54e5f9631871c831d9e
pbraswell <peter.braswell@gmail.com>
2013-12-27 11:24:34 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmazoneDrone::Application.config.secret_key_base = '733f742d050c3ff96de383734333d44fce56d27f5d6de7bee5dde097ce787a5846c97ab992853f012a11c5554ad93efd3a37375b232a80f78cf303a7bb357ce8'
